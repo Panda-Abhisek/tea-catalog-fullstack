3ebd710c710aa44264f289d5118209cc1360666d
Panda-Abhisek <abhisekpanda114@gmail.com>
2026-06-16 23:47:33 +0530

    production api config

--- tea-frontend/vite.config.js
+++ tea-frontend/vite.config.js
@@ -5,13 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Adjust to your actual backend URL
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   port: 3000,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000', // Adjust to your actual backend URL
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
